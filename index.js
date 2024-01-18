78a5b444a4f7eb07a7ab2e8a203643d29f5f4429
engindenizz <eryalcinengin@icloud.com>
2024-01-18 21:22:07 +0300

    engin/master-bug-fixed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
  document.querySelector("#package-plan-button").addEventListener("click", packageMenuOpen);
 document.querySelector("#package-menu-close").addEventListener("click", packageMenuClose);
+const bodyScroll = document.body;
 function packageMenuOpen(){
    let menu = document.querySelector("#package-menu");
    menu.style.display = "block";
+   bodyScroll.style.overflow = "hidden"
    let headerMenu = document.querySelector("#header");
    headerMenu.classList.add("homepage-before")
 
@@ -11,6 +13,7 @@ function packageMenuOpen(){
 function packageMenuClose(){
     let menu = document.querySelector("#package-menu");
     menu.style.display = "none";
+    bodyScroll.style.overflow = "scroll";
     let headerMenu = document.querySelector("#header");
     headerMenu.classList.remove("homepage-before")
  
