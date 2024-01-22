8ccfabc17c72c9c8e2a3a3efbda9b715cdcaf8f1
Altuğ Altuner <vefgend@gmail.com>
2024-01-22 12:26:26 +0300

    engin/TO-35/trip-booking-landscape

--- tour-plan.js
+++ tour-plan.js
@@ -1,11 +1,17 @@
 document.getElementById("gallery-menu").addEventListener("click", galeryOpen);
-document
-  .getElementById("tourplan-menu")
-  .addEventListener("click", tourPlanOpen);
+document.getElementById("tourplan-menu").addEventListener("click", tourPlanOpen);
+document.getElementById("location-menu").addEventListener("click", LocationOpen);
+document.getElementById("information-menu").addEventListener("click", informationOpen);
+
 const gallery = document.getElementById("gallery");
 const tourPlan = document.getElementById("tour-plan");
 const galleryMenu = document.getElementById("gallery-menu-area");
 const tourPlanMenu = document.getElementById("tourplan-menu-area");
+const booking4 = document.getElementsByClassName("booking-container-4");
+const booking3 = document.getElementsByClassName("booking-container-3");
+const booking1 = document.getElementsByClassName("booking-container-1");
+const informationmenu = document.getElementById("information-menu");
+const greenplantphoto = document.getElementsById("greenphoto");
 
 function galeryOpen() {
   event.preventDefault();
@@ -15,6 +21,7 @@ function galeryOpen() {
   tourPlan.style.display = "none";
   information.style.display = "none";
   location.style.display = "none";
+  booking1.style = "none";
 }
 function tourPlanOpen() {
   event.preventDefault();
@@ -24,4 +31,27 @@ function tourPlanOpen() {
   tourPlan.style.display = "block";
   information.style.display = "none";
   location.style.display = "none";
+  booking1.style = "none";
 }
+
+function informationOpen() {
+  event.preventDefault();
+  gallery.style.display = "none";
+  tourPlan.style.display = "none";
+  information.style.display = "block";
+  location.style.display = "none";
+  informationmenu.style.background = "#FF00FF"
+  greenplantphoto.style.display = "none";
+  booking1.style = "none";
+}
+
+function LocationOpen() {
+  event.preventDefault();
+  gallery.style.display = "none";
+  tourPlan.style.display = "none";
+  information.style.display = "none";
+  location.style.display = "block";
+  greenplantphoto.style.display = "none";
+  booking1.style = "none";
+}
+
