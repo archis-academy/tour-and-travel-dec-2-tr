10e49f50899bd0799f49e228ab36d6d87f6b3ddc
Altuğ Altuner <vefgend@gmail.com>
2024-01-22 18:50:09 +0300

    engin/TO-35/trip-booking-landscape

--- tour-plan.js
+++ tour-plan.js
@@ -1,57 +1,147 @@
-document.getElementById("gallery-menu").addEventListener("click", galeryOpen);
-document.getElementById("tourplan-menu").addEventListener("click", tourPlanOpen);
-document.getElementById("location-menu").addEventListener("click", LocationOpen);
+
+
 document.getElementById("information-menu").addEventListener("click", informationOpen);
+document.getElementById("location-menu").addEventListener("click", LocationOpen);
+document.getElementById("gallery-menu").addEventListener("click", galleryOpen);
+document.getElementById("tourplan-menu").addEventListener("click", tourPlanOpen);
 
+//bazı sayfalarda olmayan yeşillik fotoğrafı
+const greeneryphoto = document.getElementById("greenphoto");
+
+//sayfalar aşağıdadır
+const information = document.getElementById("information");
+const locationpage = document.getElementById("location-page");
 const gallery = document.getElementById("gallery");
-const tourPlan = document.getElementById("tour-plan");
-const galleryMenu = document.getElementById("gallery-menu-area");
-const tourPlanMenu = document.getElementById("tourplan-menu-area");
-const booking4 = document.getElementsByClassName("booking-container-4");
-const booking3 = document.getElementsByClassName("booking-container-3");
-const booking1 = document.getElementsByClassName("booking-container-1");
-const informationmenu = document.getElementById("information-menu");
-const greenplantphoto = document.getElementsById("greenphoto");
+const tourplan = document.getElementById("tourplan");
+const bookingsearcharea = document.getElementById("bookingarea");
 
-function galeryOpen() {
-  event.preventDefault();
-  tourPlanMenu.style.background = "#f8f8f8";
-  gallery.style.display = "block";
-  galleryMenu.style.background = "#fff";
-  tourPlan.style.display = "none";
-  information.style.display = "none";
-  location.style.display = "none";
-  booking1.style = "none";
-}
-function tourPlanOpen() {
-  event.preventDefault();
+//butonlar aşağıdadır
+const info_button = document.getElementById("information-area");
+const location_button = document.getElementById("location-menu-area");
+const gallery_button = document.getElementById("gallery-menu-area");
+const tour_button = document.getElementById("tourplan-menu-area");
+
+// let yeniGenislik = 600;
+
+//yeşillik fotosunu da gösterme
+greeneryphoto.style.display = "none";
+
+
+//başlangıçta görünen sayfa ayarlaması
+let currentPage = information;
+
+function showPage(currentPage) {
+  // Tüm sayfaları gizle
+  locationpage.style.display = "none";
   gallery.style.display = "none";
-  tourPlanMenu.style.background = "#fff";
-  galleryMenu.style.background = "#f8f8f8";
-  tourPlan.style.display = "block";
-  information.style.display = "none";
-  location.style.display = "none";
-  booking1.style = "none";
+  tourplan.style.display = "none";
+
+
+  // ilk sayfayı göster
+  information.style.display = "block";
 }
+//fonksiyonu çağırdım.
+showPage();
+
+
+
+//butonlara tıklayıp sayfa değiştirme
 
 function informationOpen() {
   event.preventDefault();
-  gallery.style.display = "none";
-  tourPlan.style.display = "none";
   information.style.display = "block";
-  location.style.display = "none";
-  informationmenu.style.background = "#FF00FF"
-  greenplantphoto.style.display = "none";
-  booking1.style = "none";
+  locationpage.style.display = "none";
+  gallery.style.display = "none";
+  tourplan.style.display = "none";
+  //renk değişimleri için
+  info_button.style.background = "#fff";
+  location_button.style.background = "#f8f8f8";
+  gallery_button.style.background = "#f8f8f8";
+  tour_button.style.background = "#f8f8f8";
+
+  greeneryphoto.style.display = "none";
 }
+
 
 function LocationOpen() {
   event.preventDefault();
-  gallery.style.display = "none";
-  tourPlan.style.display = "none";
   information.style.display = "none";
-  location.style.display = "block";
-  greenplantphoto.style.display = "none";
-  booking1.style = "none";
+  locationpage.style.display = "block";
+  gallery.style.display = "none";
+  tourplan.style.display = "none";
+  //renk değişimleri için
+  info_button.style.background = "#f8f8f8";
+  location_button.style.background = "#fff";
+  gallery_button.style.background = "#f8f8f8";
+  tour_button.style.background = "#f8f8f8";
+
+  greeneryphoto.style.display = "block";
 }
 
+function galleryOpen() {
+  event.preventDefault();
+  information.style.display = "none";
+  locationpage.style.display = "none";
+  gallery.style.display = "block";
+  tourplan.style.display = "none";
+  //renk değişimleri için
+  info_button.style.background = "#f8f8f8";
+  location_button.style.background = "#f8f8f8";
+  gallery_button.style.background = "#fff";
+  tour_button.style.background = "#f8f8f8";
+
+  greeneryphoto.style.display = "block";
+}
+
+function tourPlanOpen() {
+  event.preventDefault();
+  information.style.display = "none";
+  locationpage.style.display = "none";
+  gallery.style.display = "none";
+  tourplan.style.display = "block";
+  //renk değişimleri için
+  info_button.style.background = "#f8f8f8";
+  location_button.style.background = "#f8f8f8";
+  gallery_button.style.background = "#f8f8f8";
+  tour_button.style.background = "#fff";
+
+  greeneryphoto.style.display = "none";
+}
+
+
+// Başlangıçta görüntülenen sayfa
+// let currentPage = "information";
+
+// document.getElementById("gallery-menu").addEventListener("click", function (event) {
+//   event.preventDefault();
+//   showPage("gallery");
+// });
+
+// document.getElementById("tourplan-menu").addEventListener("click", function (event) {
+//   event.preventDefault();
+//   showPage("tour-plan");
+// });
+
+// document.getElementById("location-menu").addEventListener("click", function (event) {
+//   event.preventDefault();
+//   showPage("location");
+// });
+
+// document.getElementById("information-menu").addEventListener("click", function (event) {
+//   event.preventDefault();
+//   showPage("information");
+// });
+
+// function showPage(page) {
+//   // Tüm sayfaları gizle
+//   document.getElementById("gallery").style.display = "none";
+//   document.getElementById("tour-plan").style.display = "none";
+//   document.getElementById("location").style.display = "none";
+//   document.getElementById("information").style.display = "none";
+
+//   // Belirli sayfayı göster
+//   document.getElementById(page).style.display = "block";
+
+//   // Güncel sayfa değişkenini güncelle
+//   currentPage = page;
+// }
